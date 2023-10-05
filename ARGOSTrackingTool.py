a190bdb7288324e5b3a88d9a982ae37088e20dcb
nanditharam3 <nanditha.satagopan@duke.edu>
2023-10-05 18:40:13 -0400

    final turtle tracker script

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -56,3 +56,24 @@ print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon
 """To use formatted string literals, begin a string with f or F before the opening quotation mark or triple quotation mark in a print() statement. 
 Inside this string, you can write a Python expression between { } characters that can refer to variables or literal values. 
 It is an easier way to insert direct values with {} calling the variables rather than using a concatenation for every word"""
+
+#Asking ser for a date
+user_date = input("Pick a date in M/DD/YYYY")
+#Initialize key list
+keys = []
+
+# Loop through all key, value pairs in the date_dictionary
+for key, value in date_dict.items():
+    #See if the date (the value) matches the user date
+    if value == user_date:
+        keys.append(key)
+
+# Report whether no keys were found
+if len(keys) == 0:
+    print(f"Sara was not located on {user_date}")
+
+else:
+    #Reveal locations for each key in matching_keys
+    for key in keys:
+        lat, lng = location_dict[key]
+        print(f"On {user_date}, Sara the the turtle was seen at {lat}d Lat, {lng}d Lng.")
